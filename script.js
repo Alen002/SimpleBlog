5b1acb684a19a76ac60de45b2a836ef838c581dd
Alen <Alenslo@gmx.net>
2019-10-16 14:07:21 +0200

    create article functionality implemented

--- script.js
+++ script.js
@@ -5,26 +5,34 @@ let lastArticle = 0;
 let number = 0;
 
 
-function createArticle() {
+ function createArticle() {
  
-    number = countArticles();
+    number = 'article_'+ countArticles(); // create the class name for the new article
 
     let newArticle = document.createElement('article') //create new article
     const selectDiv = document.querySelector('div'); //select parent
 
-    newArticle.className = number;
-    selectDiv.appendChild(newArticle); //append the article   
+    newArticle.className = number; //add class name for newly created article
+    selectDiv.appendChild(newArticle); //append the article  
+
+    let newDate = document.createElement('p');
+    newArticle.appendChild(newDate).innerHTML = document.querySelector('#inputDate').value;
+    
+    let newHeadline = document.createElement('h4');
+    newArticle.appendChild(newHeadline).innerHTML = document.querySelector('#inputHeadline').value;
+    
+    let newText = document.createElement('p');
+    newArticle.appendChild(newText).innerHTML = document.querySelector('#inputText').value;
+
 };
 
-function countArticles() {
+function countArticles() { //create class for the new article
     classNumber +=1;
     console.log('countArticles() ' +classNumber);
     return classNumber;
 }
 
-
-
-function removeArticle() {
+function removeArticle() { // removes the last article created
     let a = 0;
     document.querySelectorAll('article').forEach(() => a += 1); // count all current articles
 
